2ce3c09a63b77fb1646e17c1d909e5d98174e110
Bruno <renato.romao@go-it.work>
2020-01-29 16:53:28 -0300

    Alterações

--- scriptListas.ps1
+++ scriptListas.ps1
@@ -1,70 +1,83 @@
 ﻿$Site = Read-Host 'Qual a url quer navegar?';
 
 while (!$Site) {
-   $Site = Read-Host 'Adicione uma URL!'   
+    $Site = Read-Host 'Adicione uma URL!'  
+}
+try {
+    Connect-PnPOnline -Url $Site;
+}
+catch {
+    $Site = Read-Host 'Url não encontrada, tente novamente!'
+    Connect-PnPOnline -Url "https://"+ $Site;
 }
 
-Connect-PnPOnline -Url $Site;
-
+#Definindo inputs
 $ListDe = Read-Host 'Qual lista deseja copiar?';
 $ListDe = "Lists/" + $ListDe;
 $ListPara = Read-Host 'Para qual lista deseja enviar?';
 $ListPara = "Lists/" + $ListPara;
-
+#Pegando as listas
 $sourceList = Get-PnPList -Identity $ListDe;
 $targetList = Get-PnPList -Identity $ListPara;    
-$sourceItems = Get-PnPListItem -List $ListDe;
-
+[array]$sourceItems = Get-PnPListItem -List $ListDe;
+[array]$targetItems = Get-PnPListItem -List $ListPara;
+#Array de colunas source e target
 $sourceFields = $sourceList.Fields
 $targetFields = $targetList.Fields
-
+#Carregando o contexto da lista
 $sourceList.Context.Load($sourceFields);
 $sourceList.Context.ExecuteQuery();
 $targetList.Context.Load($targetFields);
 $targetList.Context.ExecuteQuery();
-
-
-
-   $listaEncontrados = @()
-   $listaNaoEncontrados = @()
-
-   foreach ($Field in $sourceFields) {
-
-    $itemEncontrado = $targetFields | Where-Object { $_.InternalName -in $Field.InternalName -and $Field.FromBaseType -eq $false -and $_.FromBaseType -eq $false }
-    if($itemEncontrado -ne $null)
-    {
+#Listas de campos
+$listaEncontrados = @()
+$listaNaoEncontrados = @()
+#Para cada coluna nas colunas da source
+foreach ($Field in $sourceFields | Where-Object { $_.FromBaseType -eq $false }) {
+    #No Array de colunas do target, procure onde o nome interno for igual ao nome interno da source e tenha sido criado por usuário
+    $itemEncontrado = $targetFields | Where-Object { $Field.FromBaseType -eq $false -and $_.FromBaseType -eq $false };
+    $itemEncontrado = $targetFields | Where-Object { $_.InternalName -in $Field.InternalName }
+    if ($itemEncontrado -ne $Null) {
         $listaEncontrados += $itemEncontrado[0].InternalName
-    } else
-    {
+    } 
+    else {
         $listaNaoEncontrados += $Field.InternalName
     }
-   }
-   
+}
 
-   foreach ($item in $sourceItems) 
-    {
-        $jsonBase = @{}
+#No array de items da source, para cada item, criar um json vazio, e adicionando os campos
+$targetItems | ForEach-Object{
+    $titleObj = $_.Title;
+    Write-Host $titleObj;
+}
+foreach ($item in $sourceItems) {
+    $jsonBase = @{"Title" = $item["Title"]; "Modified" = $item["Modified"]; "Created" = $item["Created"]; }
+    #Para cada campo na lista de campos encontrados, adicione em um json
+    $identifyTitle = Get-PnPListItem -List $ListPara -Query "<View><Query><Where><Eq><FieldRef Name='Title'/><Value Type='Text'>$($item["Title"])</Value></Eq></Where></Query></View>";
+    $identifyTitle;
 
-        foreach($campo in $listaEncontrados)
-        {
-           $jsonBase.Add($campo, $item[$campo]); 
-        }
-        
-       Add-PnPListItem -List $ListPara -Values $jsonBase
-
-        #$jsonBase
+    foreach ($campo in $listaEncontrados) {
+        $jsonBase.Add($campo, $item[$campo]);
     }
- 
- # criar lista de campos
- # criar lista de campos não encontrados
- # iteração para cada field do Source
+    $identifyTitle.Count;
+  if ($identifyTitle.Length -gt 0) {
+        #Adicione cada item com os valores do json montado
+        Set-PnPListItem -List $ListPara -Values $jsonBase -Identity $identifyTitle.Id;
+    }
+    else {
+        Add-PnPListItem -List $ListPara -Values $jsonBase
+    }
+}
+    # criar lista de campos
+    # criar lista de campos não encontrados
+    # iteração para cada field do Source
     # Valida se o campo existe no target, usando um filtro
     # SE o campo existe
-        # adiciona o nome interno do campo na lista de campos
+    # adiciona o nome interno do campo na lista de campos
     # SENAO
-        # adiciona o nome interno do campo na lista de campos nao encontrados
-# iteração para cada item da lista de source
-# criar objeto json
-# iteração para cada campo da lista de campos encontrados
+    # adiciona o nome interno do campo na lista de campos nao encontrados
+    # iteração para cada item da lista de source
+    # criar objeto json
+    # iteração para cada campo da lista de campos encontrados
     # crair uma proprierdade no objeto json
-# rodar add-pnplistitem com o valor do json gerado
+    # rodar add-pnplistitem com o valor do json gerado
