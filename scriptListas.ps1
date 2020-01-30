1a0755dbebaff57c32ccbac150aa2a4ba0b318a4
Bruno <renato.romao@go-it.work>
2020-01-30 14:06:23 -0300

    Muitas verificacoes

--- scriptListas.ps1
+++ scriptListas.ps1
@@ -1,24 +1,25 @@
-﻿$Site = Read-Host 'Qual a url quer navegar?';
+﻿#Global
 $currentTime = $(get-date).ToString("yyyyMMddHHmmss")  
 $logFilePath = ".\log-" + $currentTime + ".docx"  
-# Fields that has to be retrieved  
-$Global:selectProperties = @("Old");
 
-#While pra validar se url é vazia
-while (!$Site) {
-    $Site = Read-Host 'Qual a url quer navegar?';  
-}
-
-function loadLists{
+function loadLists {
     param([string]$ListDe, [string]$ListPara)
     #Pegando as listas
+   
+    if ($ListDe -eq $null -or $ListDe -eq "") {
+        return "Valor inválido";
+    }
 
-     $sourceList = Get-PnPList -Identity $ListDe;
-     $targetList = Get-PnPList -Identity $ListPara;
-        if($sourceList -eq $null -or $targetList -eq $null){
-            return "Valor inválido";
-        } 
-        else{
+    if ($ListPara -eq $null -or $ListPara -eq "") {
+        return "Valor inválido";
+    }
+
+    $sourceList = Get-PnPList -Identity $ListDe;
+    $targetList = Get-PnPList -Identity $ListPara;
+    if ($sourceList -eq $null -or $targetList -eq $null -or $ListPara -eq $null -or $ListDe -eq $null) {
+        return "Valor inválido";
+    } 
+    else {
 
         [array]$sourceItems = Get-PnPListItem -List $ListDe;
         #Array de colunas source e target
@@ -66,44 +67,45 @@ function loadLists{
             foreach ($campo in $listaNaoEncontrados) {  
                 $obj = New-Object PSObject              
                 $obj | Add-Member -MemberType NoteProperty -name "CamposNaoEncontrados" -value $campo;
-                $obj | Add-Member -MemberType NoteProperty -name "NovosNomes" -value $null;
+                $obj | Add-Member -MemberType NoteProperty -name "CamposAlvo" -value " ";
                 $hashTable += $obj;  
                 $obj = $null;  
+                Write-Host "Colunas não preenchidas de $($ListDe) para a $($ListPara):", $campo -ForegroundColor Blue
             }
-            $hashtable | Export-Csv $outputFilePath -NoTypeInformation  
+            $hashtable | Export-Csv $outputFilePath -NoTypeInformation;
+            $targetFieldsEncontrados = $targetFields | Where-Object { $_.FromBaseType -eq $false };
+            foreach ($coluna in $targetFieldsEncontrados) {
+                Write-Host "Coluna presente na lista $($ListPara):", $coluna.InternalName -ForegroundColor Yellow
+            }
+            Write-Host "Um arquivo csv contendo as colunas da $($ListDe) não presentes na $($ListPara) foi criado com sucesso!" -ForegroundColor Green
+            Start-Sleep -s 2
+            Write-Host "Items enviados para a lista com sucesso!" -ForegroundColor Green
+            Start-Sleep -s 6
+
         }
         catch [Exception] {  
             $ErrorMessage = $_.Exception.Message         
             Write-Host "Error: $ErrorMessage" -ForegroundColor Red          
         } 
         Stop-Transcript
-        # criar lista de campos
-        # criar lista de campos não encontrados
-        # iteração para cada field do Source
-        # Valida se o campo existe no target, usando um filtro
-        # SE o campo existe
-        # adiciona o nome interno do campo na lista de campos
-        # SENAO
-        # adiciona o nome interno do campo na lista de campos nao encontrados
-        # iteração para cada item da lista de source
-        # criar objeto json
-        # iteração para cada campo da lista de campos encontrados
-        # crair uma proprierdade no objeto json
-        # rodar add-pnplistitem com o valor do json gerado
-        }
+    }
 }
 function tryToConnect {
     Param ([string]$siteurl)
     try {
-        Connect-PnPOnline -Url $siteurl;
+        Connect-PnPOnline -Url $siteurl -Credentials (Get-Credential);
         
         Start-Transcript -Path $logFilePath   
-
         #Definindo inputs
         $ListDe = Read-Host 'Qual lista deseja copiar?';
-        $ListDe = "Lists/" + $ListDe;
+        if ($ListDe -ne $null) {
+            $ListDe = "Lists/" + $ListDe;        
+        }
         $ListPara = Read-Host 'Para qual lista deseja enviar?';
-        $ListPara = "Lists/" + $ListPara;
+        if ($ListPara -ne $null) {
+            $ListPara = "Lists/" + $ListPara;
+        }
+
         #Pegando as listas
         $res = loadLists -ListDe $ListDe -ListPara $ListPara;
         if ($res -eq "Valor inválido") {
@@ -111,10 +113,13 @@ function tryToConnect {
             do {      
                 Write-Host "Listas não encontradas, insira novamente!" -ForegroundColor Red
                 $ListDe = Read-Host 'Qual lista deseja copiar?';
-                $ListDe = "Lists/" + $ListDe;
+                if ($ListDe -ne $null) {
+                    $ListDe = "Lists/" + $ListDe;        
+                }
                 $ListPara = Read-Host 'Para qual lista deseja enviar?';
-                $ListPara = "Lists/" + $ListPara;
-
+                if ($ListPara -ne $null) {
+                    $ListPara = "Lists/" + $ListPara;
+                }
                 $res = loadLists -ListDe $ListDe -ListPara $ListPara;
             }
             while ($res -eq "Valor inválido") 
@@ -125,14 +130,75 @@ function tryToConnect {
         return $ErrorMessage;
     }
 }
-$result = tryToConnect -siteurl $Site;
-if ($result -eq "UriFormatException") {
+  
+
+Write-host "A segunda lista está em outro ambiente do sharepoint? (Padrão: Não)" -ForegroundColor Yellow 
+$userChoice = Read-Host " ( S / N ) "
+Switch ($userChoice) { 
+    S {
+        $Site = Read-Host 'Qual a url do site de onde a lista é enviada?';
+        #While pra validar se url é vazia
+        while (!$Site) {
+            $Site = Read-Host 'Qual a url do site de onde a lista é enviada?';
+        }
+        $Site2 = Read-Host 'Qual a url do site para qual a lista será enviada?';
+        #While pra validar se url é vazia
+        while (!$Site2) {
+            $Site2 = Read-Host 'Qual a url do site para qual a lista será enviada?';
+        }
+
+    } 
+    N {
+        $Site = Read-Host 'Qual a url quer navegar?';
+        #While pra validar se url é vazia
+        while (!$Site) {
+            $Site = Read-Host 'Qual a url quer navegar?';  
+        }
+
+        $result = tryToConnect -siteurl $Site;
+        if ($result -eq "UriFormatException" -or $result -eq "WebException" -or $result -eq "IdcrlException") {
     
-    do {
-        Write-Host "Url não encontrada!" -ForegroundColor Red      
-        $retry = Read-Host 'Qual a url quer navegar?'; 
-        $res = tryToConnect -siteurl $retry
-    }
-    while ($res -eq "UriFormatException") 
-};
+            do {
+                $retry = Read-Host 'Qual a url quer navegar?'; 
+                $res = tryToConnect -siteurl $retry
+                if ($res -eq "UriFormatException") {
+                    Write-Host "Url não válida!" -ForegroundColor Red      
+                }
+                if ($res -eq "WebException") {
+                    Write-Host "As credenciais ou URL estão inválidas" -ForegroundColor Red      
+                }
+                if ($res -eq "IdcrlException") {
+                    Write-Host "As credenciais estão inválidas" -ForegroundColor Red 
+                }
+            }
+            while ($res -eq "UriFormatException" -or $res -eq "WebException" -or $res -eq "IdcrlException") 
+        };
+    } 
+    Default {
+        $Site = Read-Host 'Qual a url quer navegar?';
+        #While pra validar se url é vazia
+        while (!$Site) {
+            $Site = Read-Host 'Qual a url quer navegar?';  
+        }
+
+        $result = tryToConnect -siteurl $Site;
+        if ($result -eq "UriFormatException" -or $result -eq "WebException" -or $result -eq "IdcrlException") {
+    
+            do {
+                $retry = Read-Host 'Qual a url quer navegar?'; 
+                $res = tryToConnect -siteurl $retry
+                if ($res -eq "UriFormatException") {
+                    Write-Host "Url não válida!" -ForegroundColor Red      
+                }
+                if ($res -eq "WebException") {
+                    Write-Host "As credenciais ou URL estão inválidas" -ForegroundColor Red      
+                }
+                if ($res -eq "IdcrlException") {
+                    Write-Host "As credenciais estão inválidas" -ForegroundColor Red 
+                }
+            }
+            while ($res -eq "UriFormatException" -or $res -eq "WebException" -or $res -eq "IdcrlException") 
+        };
+    } 
+}
 
