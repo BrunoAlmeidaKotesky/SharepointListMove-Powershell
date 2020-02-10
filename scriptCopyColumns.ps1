d9e5e810d349c070db4ba7b315590624b508495e
Bruno <renato.romao@go-it.work>
2020-02-10 12:21:39 -0300

    Ta tudo funcionando

--- scriptCopyColumns.ps1
+++ scriptCopyColumns.ps1
@@ -4,12 +4,13 @@ function retryConnection {
 
     try {
         if ($true -eq $isExternal -and $null -ne $siteurl) {
-            Write-Host "Insira as credenciais da primeira URL!" -ForegroundColor Yellow
+            #Add-PnPStoredCredential -Name $siteurl;             
+            Write-Host "Insira as credenciais da URL!" -ForegroundColor Yellow
             Connect-PnPOnline -Url $siteurl;
             return "MultipleConnection";
         }
         elseif ($false -eq $isExternal -or $null -eq $isExternal) {
-            Write-Host "Insira as credenciais da primeira URL!" -ForegroundColor Yellow
+            Write-Host "Insira as credenciais da URL!" -ForegroundColor Yellow
             Connect-PnPOnline -Url $siteurl;
             return "SingleConnection";
         }
@@ -70,7 +71,7 @@ function userOptions {
                 if ($null -ne $ListPara -and $null -ne $ListDe) {
                     #Executa a copyListandCreate
                     $res = copyAndCreateList -ListDe $ListDe -ListPara $ListPara -segundoSite $Site2;
-                    if ($res -eq "Valor inválido") {
+                    if ($res -eq "ListAlreadyExists") {
             
                         do {      
                             Write-Host "Listas não encontradas, insira novamente!" -ForegroundColor Red
@@ -84,8 +85,24 @@ function userOptions {
                             }
                             $res = copyAndCreateList -ListDe $ListDe -ListPara $ListPara -segundoSite $retry2;
                         }
-                        while ($res -eq "Valor inválido") 
-                    };
+                        while ($res -eq "ListAlreadyExists" -or $res -eq "Valor Inválido") 
+                    }
+                    elseif($res -eq "TenantDisconnected"){
+
+                        do {      
+                            Write-Host "Listas não encontradas, insira novamente!" -ForegroundColor Red
+                            $ListDe = Read-Host 'Qual lista deseja copiar?';
+                            if ($ListDe -ne $null) {
+                                $ListDe = "Lists/" + $ListDe;        
+                            }
+                            $ListPara = Read-Host 'Qual o nome da lista que deseja criar??';
+                            if ($ListPara -ne $null) {
+                                $ListPara = "Lists/" + $ListPara;
+                            }
+                            $res = copyAndCreateList -ListDe $ListDe -ListPara $ListPara -firstUrl $retry -segundoSite $retry2 -lostContext $true;
+                        }
+                        while ($res -eq "TenantDisconnected")
+                    }
 
                 }
             }
@@ -131,8 +148,8 @@ function userOptions {
         if ($null -ne $ListPara -and $null -ne $ListDe) {
             #Executa a copyListandCreate
             $res = copyAndCreateList -ListDe $ListDe -ListPara $ListPara -segundoSite $Site2;
-            if ($res -eq "Valor inválido") {
-    
+            if ($res -eq "ListAlreadyExists") {
+                
                 do {      
                     Write-Host "Listas não encontradas, insira novamente!" -ForegroundColor Red
                     $ListDe = Read-Host 'Qual lista deseja copiar?';
@@ -145,8 +162,24 @@ function userOptions {
                     }
                     $res = copyAndCreateList -ListDe $ListDe -ListPara $ListPara -segundoSite $Site2;
                 }
-                while ($res -eq "Valor inválido") 
-            };
+                while ($res -eq "ListAlreadyExists" -or $res -eq "Valor Inválido") 
+            }
+            elseif ($res -eq "TenantDisconnected"){
+
+                do {      
+                    Write-Host "Listas não encontradas, insira novamente!" -ForegroundColor Red
+                    $ListDe = Read-Host 'Qual lista deseja copiar?';
+                    if ($ListDe -ne $null) {
+                        $ListDe = "Lists/" + $ListDe;        
+                    }
+                    $ListPara = Read-Host 'Qual o nome da lista que deseja criar??';
+                    if ($ListPara -ne $null) {
+                        $ListPara = "Lists/" + $ListPara;
+                    }
+                    $res = copyAndCreateList -ListDe $ListDe -ListPara $ListPara -firstUrl $Site -segundoSite $Site2 -lostContext $true;
+                }
+                while ($res -eq "TenantDisconnected")
+            }
 
         }
     }
@@ -176,13 +209,19 @@ function userOptions {
                     $res = copyAndCreateList -ListDe $ListDe -ListPara $ListPara;
                 }
                 while ($res -eq "Valor inválido") 
-            };
+            }
         }
     }
 }
 
+function disconnectAndSetStatus {
+    Disconnect-PnPOnline;
+    $isDisconnect = $true;
+    return $isDisconnect;
+}
+
 function copyAndCreateList {
-    param([string]$ListDe , [string]$ListPara, [string]$segundoSite)
+    param([string]$ListDe, [string]$ListPara, [string]$segundoSite, [bool]$lostContext, [string]$firstUrl)
     #Verificando se o valor da lista é nulo
     if ($ListDe -eq $null -or $ListDe -eq "") {
         return "Valor inválido";
@@ -192,18 +231,38 @@ function copyAndCreateList {
         return "Valor inválido";
     }
     else {
+        if($true -eq $lostContext){
+            $tenant1 = retryConnection -siteurl $firstUrl -isExternal $true;
+            if ($tenant1 -eq "UriFormatException") {
+                Write-Host "Url não válida!" -ForegroundColor Red      
+            }
+            if ($tenant1 -eq "WebException") {
+                Write-Host "As credenciais ou URL estão inválidas" -ForegroundColor Red      
+            }
+            if ($tenant1 -eq "IdcrlException") {
+                Write-Host "As credenciais estão inválidas" -ForegroundColor Red 
+            }
+            if($tenant1 -eq "MultipleConnection"){
+                $sourceList = Get-PnPList -Identity $ListDe;
+                $allSourceFields =Get-PnPField -List $ListDe
+                $sourceFields = $allSourceFields | Where-Object { $_.FromBaseType -eq $false };
+            }
+        }
+
 
         $sourceList = Get-PnPList -Identity $ListDe;
         $allSourceFields =Get-PnPField -List $ListDe
         $sourceFields = $allSourceFields | Where-Object { $_.FromBaseType -eq $false };
         #Carregando o contexto da lista
-        $listExists = Get-PnPList -Identity $ListPara;
+        
 
-        if ($null -eq $sourceList -or $ListPara -eq $null -or $ListDe -eq $null) { return "Valor inválido"; } 
+        
         #Se for no mesmo tenant
         if ($null -eq $segundoSite -or $segundoSite -eq "") {
-            if($null -ne $listExists){
-                $novaLista = New-PnPList -Title $ListPara -Template GenericList;
+            if ($null -eq $sourceList) { return "Valor inválido"; } 
+            $listExists = Get-PnPList -Identity $ListPara.Replace("Lists/", "");
+            if($null -eq $listExists){
+                $novaLista = New-PnPList -Title $ListPara.Replace("Lists/", "") -Template GenericList;
                 try {
                     foreach ($field in $sourceFields | Where-Object { $_.FromBaseType -eq $false }) {
                         if($field.InternalName -ne "Title" -or $field.InternalName -ne "Modified" -or $field.InternalName -ne "Created"){
@@ -227,12 +286,13 @@ function copyAndCreateList {
         }
         #Se for em mais de um tenanat
         else {
+            if ($null -eq $sourceList) { return "TenantDisconnected"; } 
             Disconnect-PnPOnline;
             $connectionRes = retryConnection -siteurl $segundoSite -isExternal $true;
             if ($connectionRes -eq "UriFormatException" -or $connectionRes -eq "WebException" -or $connectionRes -eq "IdcrlException") {
                 Write-Host "A url ou credenciais informadas para o site alvo estão inválidas, tente novamente!" -ForegroundColor Red;
                 do {
-                    $retry = Read-Host 'Qual a url do site para qual a lista será enviada?';
+                    $retry = Read-Host 'Qual a url do site para qual a lista será  criada??';
     
                     $res = retryConnection -siteurl $retry -isExternal $true;
                     if ($res -eq "UriFormatException") {
@@ -244,12 +304,37 @@ function copyAndCreateList {
                     if ($res -eq "IdcrlException") {
                         Write-Host "As credenciais estão inválidas" -ForegroundColor Red 
                     }
+                    if($res -eq "MultipleConnection"){
+                        $listExists = Get-PnPList -Identity $ListPara.Replace("Lists/", "");
+                        if($null -eq $listExists){
+                            $novaLista = New-PnPList -Title $ListPara -Template GenericList;
+                        try {
+                            foreach ($field in $sourceFields | Where-Object { $_.FromBaseType -eq $false }) {
+                                if($field.InternalName -ne "Title" -or $field.InternalName -ne "Modified" -or $field.InternalName -ne "Created"){
+                                    if($field.Required -eq $true){
+                                        $novaColuna = Add-PnPField -List $ListPara -DisplayName $field.Title -Required -Type $field.TypeAsString  -InternalName $field.InternalName;
+                                    }
+                                    else{
+                                        $novaColuna = Add-PnPField -List $ListPara -DisplayName $field.Title -Type $field.TypeAsString  -InternalName $field.InternalName;
+                                    }
+                                }
+                            }
+                        }
+                        catch [Excption]{
+                            Remove-PnPList -Identity $ListPara -Force;
+                            return $_;
+                        }
+                        }
+                        else{ Write-Host "Já existe uma lísta com esse nome!" -ForegroundColor Red
+                              return "ListAlreadyExists";
+                        }
+                    }
                 }
                 while ($res -eq "UriFormatException" -or $res -eq "WebException" -or $res -eq "IdcrlException") 
             }
             elseif ($connectionRes -eq "MultipleConnection") {
-                
-                if($null -ne $listExists){
+                $listExists = Get-PnPList -Identity $ListPara.Replace("Lists/", "");
+                if($null -eq $listExists){
                     $novaLista = New-PnPList -Title $ListPara -Template GenericList;
                 try {
                     foreach ($field in $sourceFields | Where-Object { $_.FromBaseType -eq $false }) {
@@ -269,7 +354,7 @@ function copyAndCreateList {
                 }
                 }
                 else{ Write-Host "Já existe uma lísta com esse nome!" -ForegroundColor Red
-                      return "Valor inválido";
+                      return "ListAlreadyExists";
                 }
             }
         }
